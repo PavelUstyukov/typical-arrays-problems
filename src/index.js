6f262dbd44be7de9f1977ad51f729fae78046402
Pavel Ustyukov <Pavel.Ustukov@yandex.ru>
2020-02-29 10:30:06 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || !array.length) return 0;
+    return Math.min.apply(Math, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || !array.length) return 0;
+    return Math.max.apply(Math, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0;
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) sum += parseInt(array[i], 10);
+
+    return sum / array.length;
 }
